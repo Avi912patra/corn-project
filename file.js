f9cfccbf9636e130cd584ab044715c559eb2e44f
Deeptirtha <tirtha1111222@gmail.com>
2023-02-12 19:56:28 +0530

    done

--- file.js
+++ file.js
@@ -4,25 +4,67 @@ const router = express. Router();
 
 router.post("/schedule", (_, res) => {
 try {
-let date = new Date();
-let min = date.getMinutes();
-let cuurrentdate =date.getDate();
-let month = date.getMonth(); 
-let hour =date.getHours();
-//console.log(`${min} ${cuurrentdate } ${hour} ${month} ${date}`)
 
-cron.schedule( `${min + 1} ${hour} ${cuurrentdate} ${month+1} * `, ()=>{
- console.log("task 1 completed")
+    let obj = [
+        {
+            text: "One",
+            time: "2023-02-12 19:55:00"
+        },
+        {
+            text: "Two",
+            time: "2023-02-12 19:55:03"
+        },
+        {
+            text: "Three",
+            time: "2023-02-12 19:55:06"
+        },
+        {
+            text: "Four",
+            time: "2023-02-12 19:55:09"
+        },
+        {
+            text: "Five",
+            time: "2023-02-12 19:55:12"
+        },
+        {
+            text: "Six",
+            time: "2023-02-12 19:55:15"
+        }
+    ]
+    
+    
+    
+    
+    obj.forEach(x => {
+        let timeString = x.time
+        let [dateString, time] = timeString.split(" ")
+    
+        let [year, month, date] = dateString.split("-")
+        let [hour, min, sec] = time.split(":")
+    
+        cron.schedule(`${sec} ${min} ${hour} ${date} ${month} *`, () => {
+            console.log(x.text)
+        })
+    })
+// let date = new Date();
+// let min = date.getMinutes();
+// let cuurrentdate =date.getDate();
+// let month = date.getMonth(); 
+// let hour =date.getHours();
+// //console.log(`${min} ${cuurrentdate } ${hour} ${month} ${date}`)
 
-}) 
-cron.schedule( `${min + 2} ${hour} ${cuurrentdate} ${month+1} * `, ()=>{
-    console.log("task 2 completed")
+// cron.schedule( `${min + 1} ${hour} ${cuurrentdate} ${month+1} * `, ()=>{
+//  console.log("task 1 completed")
+
+// }) 
+// cron.schedule( `${min + 2} ${hour} ${cuurrentdate} ${month+1} * `, ()=>{
+//     console.log("task 2 completed")
    
-}) 
-cron.schedule( `${min + 3} ${hour} ${cuurrentdate} ${month+1} * `, ()=>{
-    console.log("task 3 completed")
+// }) 
+// cron.schedule( `${min + 3} ${hour} ${cuurrentdate} ${month+1} * `, ()=>{
+//     console.log("task 3 completed")
    
-}) 
+// }) 
 
 res.status(201).send({msg:"task scheduled"})
 }
@@ -32,3 +74,9 @@ catch (error)
 
 module.exports = router;
 
+let date = new Date();
+let min = date.getMinutes();
+let cuurrentdate =date.getDate();
+let month = date.getMonth(); 
+let hour =date.getHours();
+console.log(min);
